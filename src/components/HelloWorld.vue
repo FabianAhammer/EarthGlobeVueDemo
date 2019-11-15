68bfc282278c3dd99cd1bb32c6fd759080e7851c
FabianAhammer <f.ahammer@students.htl-leonding.ac.at>
2019-11-15 23:24:15 +0100

    Actual implementation of the Globe

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,55 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="bg">
+    <vgl-renderer class="maxed" antialias :alpha="true">
+      <vgl-scene background-texture="require('../assets/heaven2.jpeg')">
+        <vgl-sphere-geometry :width-segments="30" :height-segments="30"></vgl-sphere-geometry>
+        <vgl-texture :src="require('../assets/earth.jpg')" center="0 0" name="texture"></vgl-texture>
+        <vgl-mesh-standard-material map="texture"></vgl-mesh-standard-material>
+        <vgl-mesh></vgl-mesh>
+        <vgl-ambient-light></vgl-ambient-light>
+        <vgl-directional-light position="90 `${y%1}` 42"></vgl-directional-light>
+      </vgl-scene>
+      <vgl-perspective-camera :orbit-position="`${x} ${y} ${z}`"></vgl-perspective-camera>
+    </vgl-renderer>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      location: 0,
+      x: 4,
+      y: 1,
+      z: 0
+    };
+  },
+  methods: {
+    rotate() {
+      if (this.z > -10) this.z -= 0.001;
+      else this.z = 0;
+    }
+  },
+  mounted() {
+    setInterval(this.rotate, 45);
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.maxed {
+  height: 100vh;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+::v-deep iframe {
+  display: none;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.bg {
+  background-image: url("../assets/heaven.jpg");
+  background-size: cover;
 }
 </style>
